08cfa8e5668d9ab84d0108bb635fa7d1ad8bc5ce
Boris <19noodles89@gmail.com>
2019-11-13 12:27:21 +0300

    fix conflicts

---
+++ dist/page-index.php
@@ -0,0 +1,11 @@
+<? include($_SERVER["DOCUMENT_ROOT"] . '/dist/include/header.php'); ?>
+
+<div class="wrapper"><h1>Hello World</h1></div>
+
+<main class="wrapper">
+	<section class="is-section"></section>
+	<section class="is-section"></section>
+	<section class="is-section"></section>
+</main>
+
+<? include($_SERVER["DOCUMENT_ROOT"] . '/dist/include/footer.php'); ?>
